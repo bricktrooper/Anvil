1cd67f9e8f5b9e45d09252e1845d934194a1e079
bricktrooper <kylepinto@yahoo.ca>
2022-03-01 11:36:30 -0500

    Config file loading and verification

--- anvil.py
+++ anvil.py
@@ -1,3 +1,27 @@
+import json
+import os
+import json
+
+from sys import argv
+
+# ===================== CONSTANTS ===================== #
+
+ERROR = -1
+SUCCESS = 0
+
+CONFIG_FILE_NAME = "anvil.json"
+CONFIG_FIELDS = {
+	"ART": str,
+	"EXE": str,
+	"CC": str,
+	"CCFLAGS": list,
+	"LDFLAGS": list,
+	"LDLIBS": list,
+	"DIR": list
+}
+
+# ===================== LOGS ===================== #
+
 RED     = "\033[0;31m"
 GREEN   = "\033[0;32m"
 YELLOW  = "\033[0;33m"
@@ -6,20 +30,62 @@ MAGENTA = "\033[0;35m"
 CYAN    = "\033[0;36m"
 RESET   = "\033[0m"
 
-def error(message):
+def log_error(message):
 	print(f"{RED}X{RESET} {message}")
 
-def warning(message):
+def log_warning(message):
 	print(f"{YELLOW}!{RESET} {message}")
 
-def success(message):
+def log_success(message):
 	print(f"{GREEN}~{RESET} {message}")
 
-def debug(message):
+def log_debug(message):
 	print(f"{CYAN}#{RESET} {message}")
 
-def info(message):
+def log_info(message):
 	print(f"{BLUE}>{RESET} {message}")
 
-def note(message):
+def log_note(message):
 	print(f"{MAGENTA}@{RESET} {message}")
+
+# ===================== CONFIG ===================== #
+
+def load_config(path):
+	if not os.path.exists(path):
+		log_error(f"Cannot find configuration file '{path}'")
+		return ERROR
+	file = open(path, "r")
+	config = json.load(file)
+	file.close()
+	log_success(f"Loaded build configuration from '{path}'")
+	return config
+
+def verify_config(config):
+	for field in CONFIG_FIELDS.keys():
+		if field not in config.keys():
+			log_error(f"Configuration field '{field}' does not exist")
+			return ERROR
+
+	for field in config.keys():
+		if field not in CONFIG_FIELDS.keys():
+			log_warning(f"Ignoring unknown configuration field '{field}'")
+		elif type(config[field]) != CONFIG_FIELDS[field]:
+			log_error(f"Incorrect type '{type(config[field])}' for configuration field '{field}' ({CONFIG_FIELDS[field]})")
+			return ERROR
+
+	return SUCCESS
+
+# ===================== MAIN ===================== #
+
+def main():
+	config = load_config(CONFIG_FILE_NAME)
+	if config == ERROR:
+		return ERROR
+	print(config)
+
+	if verify_config(config) == ERROR:
+		return ERROR
+
+	return SUCCESS
+
+exit(main())
