4d3a073a7ce6f5064b21f84b0746a392a361c9e8
bricktrooper <kylepinto@yahoo.ca>
2022-03-01 21:35:17 -0500

    Discover C source files

--- anvil.py
+++ anvil.py
@@ -1,8 +1,5 @@
-import json
 import os
 import json
-
-from sys import argv
 
 # ===================== CONSTANTS ===================== #
 
@@ -10,6 +7,7 @@ ERROR = -1
 SUCCESS = 0
 
 CONFIG_FILE_NAME = "anvil.json"
+
 CONFIG_FIELDS = {
 	"ART": str,
 	"EXE": str,
@@ -17,7 +15,7 @@ CONFIG_FIELDS = {
 	"CCFLAGS": list,
 	"LDFLAGS": list,
 	"LDLIBS": list,
-	"DIR": list
+	"SRC": list
 }
 
 # ===================== LOGS ===================== #
@@ -52,39 +50,71 @@ def log_note(message):
 
 def load_config(path):
 	if not os.path.exists(path):
-		log_error(f"Cannot find configuration file '{path}'")
+		log_error(f"Cannot find config file '{path}'")
 		return ERROR
 	file = open(path, "r")
 	config = json.load(file)
 	file.close()
-	log_success(f"Loaded build configuration from '{path}'")
+	log_success(f"Loaded build config from '{path}'")
 	return config
 
 def verify_config(config):
 	for field in CONFIG_FIELDS.keys():
 		if field not in config.keys():
-			log_error(f"Configuration field '{field}' does not exist")
+			log_error(f"'{field}' not found in build configuration")
 			return ERROR
 
 	for field in config.keys():
 		if field not in CONFIG_FIELDS.keys():
-			log_warning(f"Ignoring unknown configuration field '{field}'")
-		elif type(config[field]) != CONFIG_FIELDS[field]:
-			log_error(f"Incorrect type '{type(config[field])}' for configuration field '{field}' ({CONFIG_FIELDS[field]})")
+			log_warning(f"Ignoring unknown field '{field}' in build configuration")
+			continue
+
+		actual_type = type(config[field])
+		expected_type = CONFIG_FIELDS[field]
+
+		if actual_type != expected_type:
+			log_error(f"Incorrect type '{actual_type}' for '{field}' ({expected_type})")
 			return ERROR
 
+		if actual_type == list:
+			for item in config[field]:
+				if type(item) != str:
+					log_error(f"'{field}' must only contain strings")
+					return ERROR
+
 	return SUCCESS
+
+# ===================== DEPENDENCIES ===================== #
+
+def discover_source_files(path):
+	sources = []
+	if not os.path.isdir(path):
+		log_error(f"Source path '{path}' is not a directory'")
+		return ERROR
+
+	log_debug(f"Searching '{path}/'")
+	for file in os.listdir(path):
+		file = f"{path}/{file}"
+
+		if os.path.isdir(file):
+			sources += discover_source_files(file)
+		elif file.endswith(".c"):
+			sources.append(file)
+			log_debug(f"Discovered '{file}'")
+	return sources
 
 # ===================== MAIN ===================== #
 
 def main():
 	config = load_config(CONFIG_FILE_NAME)
-	if config == ERROR:
+	if config == ERROR or verify_config(config) == ERROR:
 		return ERROR
-	print(config)
 
-	if verify_config(config) == ERROR:
-		return ERROR
+	sources = []
+	for path in config['SRC']:
+		sources += discover_source_files(path)
+
+	print(sources)
 
 	return SUCCESS
 
