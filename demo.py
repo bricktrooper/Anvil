ad9fda233ec4afe63b44724aa37147f437e5ce2d
bricktrooper <kylepinto@yahoo.ca>
2022-03-02 01:04:11 -0500

    Full logs

---
+++ demo.py
@@ -0,0 +1,16 @@
+import log
+
+log.trace(file = False, line = False, caller = True)
+
+def test():
+	log.error("ERROR")
+	log.warning("WARNING")
+	log.success("SUCCESS")
+
+test()
+log.info("INFO")
+log.debug("DEBUG")
+
+log.suppress(log.Level.ERROR)
+log.error("ERROR")
+log.note("NOTE")
